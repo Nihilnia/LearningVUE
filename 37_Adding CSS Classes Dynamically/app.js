24ff039b8d911cf5814165fe0bc4b6f83c4d887f
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-07-30 20:20:41 +0200

    We are getting progress

--- 37_Adding CSS Classes Dynamically/app.js
+++ 37_Adding CSS Classes Dynamically/app.js
@@ -4,16 +4,20 @@ const Gloria = Vue.createApp({
       boxASelected: false,
       boxBSelected: false,
       boxCSelected: false,
+      boxDSelected: false,
     };
   },
   methods: {
     boxSelected(box) {
+      console.log(box);
       if (box === "A") {
         this.boxASelected = !this.boxASelected;
       } else if (box === "B") {
         this.boxBSelected = !this.boxBSelected;
       } else if (box === "C") {
         this.boxCSelected = !this.boxCSelected;
+      } else if (box === "D") {
+        this.boxDSelected = !this.boxDSelected;
       }
     },
   },
--- 38_Classes and Computed Properties/app.js
+++ 38_Classes and Computed Properties/app.js
@@ -4,17 +4,35 @@ const Gloria = Vue.createApp({
       boxASelected: false,
       boxBSelected: false,
       boxCSelected: false,
+      boxDSelected: false,
     };
   },
   methods: {
     boxSelected(box) {
+      console.log(box);
       if (box === "A") {
         this.boxASelected = !this.boxASelected;
       } else if (box === "B") {
         this.boxBSelected = !this.boxBSelected;
       } else if (box === "C") {
         this.boxCSelected = !this.boxCSelected;
+      } else if (box === "D") {
+        this.boxDSelected = !this.boxDSelected;
       }
+    },
+  },
+  computed: {
+    boxAClasses() {
+      return { makeItRed: this.boxASelected };
+    },
+    boxBClasses() {
+      return { makeItRed: this.boxBSelected };
+    },
+    boxCClasses() {
+      return { makeItRed: this.boxCSelected };
+    },
+    boxDClasses() {
+      return { makeItRed: this.boxDSelected };
     },
   },
 });
