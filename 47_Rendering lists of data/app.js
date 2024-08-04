dcca49fbf5159f3f7fc25c896eb5145cfa45d6f3
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-08-04 22:26:31 +0200

    v-for babe

---
+++ 47_Rendering lists of data/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return { goals: ["Vue it", "Nihil"], newGoal: "" };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.newGoal);
+      console.log(this.goals);
+      this.newGoal = "";
+    },
+    goalsLength() {
+      console.log(this.goals.length);
+      return this.goals.length === 0 ? true : false;
+    },
+  },
+  computed: {
+    allGoals() {
+      return this.goals;
+    },
+  },
+});
+
+app.mount("#user-goals");
+
+// let exArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 112];
+
+// for (f in exArray) {
+//   console.log(f);
+// }
