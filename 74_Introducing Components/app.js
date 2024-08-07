5368980a58cf16793d97861bbf4456fd8edb30bf
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-08-07 13:55:56 +0200

    E X P A N D I N '

--- 74_Introducing Components/app.js
+++ 74_Introducing Components/app.js
@@ -25,4 +25,34 @@ const App = Vue.createApp({
   },
 });
 
+App.component("try-component", {
+  data() {
+    return {
+      isShow: false,
+      person: {
+        name: "Gloria Nia",
+        phone: "00112233",
+        eMail: "un@known.com",
+      },
+    };
+  },
+  methods: {
+    toggleShow() {
+      this.isShow = !this.isShow;
+    },
+  },
+  template: `
+  <li>
+    <h2>{{person.name}}</h2>
+          <button @click="toggleShow">
+            {{isShow ? "Hide": "Show"}} Details
+          </button>
+          <ul v-if="isShow">
+            <li><strong>Phone:</strong> {{person.phone}}</li>
+            <li><strong>Email:</strong> {{person.eMail}}</li>
+          </ul>
+        </li>
+  `,
+});
+
 App.mount("#app");
