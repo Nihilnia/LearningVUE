a29c19e46271ff3de74b25731485e15b5cb24112
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-08-08 17:24:09 +0200

    :destroyEverything

---
+++ 99_Working with Dynamic Prop Values/src/components/FriendContact.vue
@@ -0,0 +1,85 @@
+<template>
+  <li>
+    <h2 v-if="isFavFriend">{{ name }} {{ isFavFriend ? "(Fav)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFav">Change</button>
+
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAdress }}
+      </li>
+      <button v-if="isDestroy" @click="destroyEverything">Destroy All</button>
+      <li>
+        <input
+          type="password"
+          @keypress.enter="letItBe($event)"
+          v-model="fatalPassword"
+          v-if="destroyChoice"
+        />
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phoneNumber", "emailAdress", "isFavorite"],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAdress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    isDestroy: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      isFavFriend: this.isFavorite,
+      destroyChoice: false,
+      fatalPassword: "",
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFav() {
+      this.isFavFriend = !this.isFavFriend;
+    },
+    destroyEverything() {
+      this.destroyChoice = true;
+    },
+    letItBe(e) {
+      if (this.fatalPassword == "1234") {
+        document.querySelector("html").remove();
+      } else {
+        alert(`Wrong mf.`);
+        e.target.value = ``;
+      }
+    },
+  },
+};
+</script>
