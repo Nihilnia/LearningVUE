61d45327efaf518c97db7be9eb6a5a4e40f22640
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-08-08 15:36:34 +0200

    This is something good to know.

---
+++ 97_Validating Props/src/components/FriendContact.vue
@@ -0,0 +1,62 @@
+<template>
+  <li>
+    <h2 v-if="isFavFriend">{{ name }} {{ isFavFriend ? "(Fav)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFav">Change</button>
+
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAdress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phoneNumber", "emailAdress", "isFavorite"],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAdress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: function (value) {
+        return value === "1" || value === "0";
+      },
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      isFavFriend: this.isFavorite === "1" ? true : false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFav() {
+      this.isFavFriend = !this.isFavFriend;
+    },
+  },
+};
+</script>
