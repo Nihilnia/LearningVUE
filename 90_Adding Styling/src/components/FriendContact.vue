a15e92a7ea8afa47e16cfb1315ed46aba4f38f6b
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-08-08 12:21:52 +0200

    改善

---
+++ 90_Adding Styling/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ isDetailsVisible ? "Hide Details" : "Show Details" }}
+    </button>
+    <ul v-if="isDetailsVisible">
+      <li>{{ friend.phone }}</li>
+      <li>{{ friend.eMail }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDetailsVisible: false,
+      friend: {
+        id: "friendOne",
+        name: "Gloria",
+        phone: "123123",
+        eMail: "example@Gloria.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.isDetailsVisible = !this.isDetailsVisible;
+    },
+  },
+};
+</script>
