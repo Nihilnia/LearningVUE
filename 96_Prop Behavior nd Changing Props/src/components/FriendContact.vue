2dc4ccad8817da1b6f50ae29dd9f8618796068e1
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-08-08 15:01:46 +0200

    Unidirectional data flow

---
+++ 96_Prop Behavior nd Changing Props/src/components/FriendContact.vue
@@ -0,0 +1,40 @@
+<template>
+  <li>
+    <h2 v-if="isFavFriend">{{ name }} {{ isFavFriend ? "(Fav)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFav">Change</button>
+
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAdress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["name", "phoneNumber", "emailAdress", "isFavorite"],
+  data() {
+    return {
+      detailsAreVisible: false,
+      isFavFriend: this.isFavorite === "1" ? true : false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFav() {
+      this.isFavFriend = !this.isFavFriend;
+    },
+  },
+};
+</script>
