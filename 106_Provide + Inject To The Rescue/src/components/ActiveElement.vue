3227ae0ac6c651256613779faf8dedce682c49c1
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-08-12 19:28:34 +0200

    I dont get it.

---
+++ 106_Provide + Inject To The Rescue/src/components/ActiveElement.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h2>{{ topicTitle }}</h2>
+    <p>{{ text }}</p>
+    <hr />
+    <p>{{ losingIt }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  inject: ['losingIt'],
+  props: ['topicTitle', 'text'],
+};
+</script>
