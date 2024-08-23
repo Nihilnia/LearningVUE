db66003fc778d1d6bdd6a35e0eb7ed8802a9af7d
Nihil <35506793+Nihilnia@users.noreply.github.com>
2024-08-23 13:00:39 +0200

    I. Loved. It.

---
+++ 106_Provide + Inject To The Rescue_2/src/components/NewElement.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <h2>Add New Topic</h2>
+    <input type="text" placeholder="Topic Name" v-model="topicName" />
+    <input
+      type="text"
+      placeholder="Topic Description"
+      v-model="topicDescripton"
+    />
+    <input
+      type="text"
+      placeholder="Full Description"
+      v-model="fullDescription"
+    />
+    <button @click="addNewTopic">Add</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      topicName: null,
+      topicDescripton: null,
+      fullDescription: null,
+    };
+  },
+  methods: {
+    addNewTopic() {
+      this.$emit(
+        'add-new-topic',
+        this.topicName,
+        this.topicDescripton,
+        this.fullDescription
+      );
+    },
+  },
+};
+</script>
